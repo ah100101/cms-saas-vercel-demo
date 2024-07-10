58b5db616505bac31ff46c2701eb113818cc4b04
Alex <alex.hawley@vercel.com>
2024-07-10 14:03:31 -0500

    updated flag property

--- apps/frontend/src/flags.ts
+++ apps/frontend/src/flags.ts
@@ -2,5 +2,7 @@ import { unstable_flag as flag } from "@vercel/flags/next";
 
 export const showNewTextBlock = flag({
   key: "show-new-text-block",
-  decide: () => false,
+  decide: () => {
+    return false;
+  },
 });
